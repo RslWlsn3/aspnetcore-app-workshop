c36587f0e73148cf760fdd934c75b668893e37d0
Connor Mote <cmote@rdgfilings.com>
2021-09-12 20:42:47 -0600

    Added speaker controller

---
+++ BackEnd/Controllers/SpeakersController.cs
@@ -0,0 +1,55 @@
+﻿using BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeakersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SpeakersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<SpeakersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SpeakersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SpeakersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SpeakersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SpeakersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
